094dfe9d9eafaf6774563ad8864521d94eb4289e
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-08 20:24:38 -0400

    working on the api

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
 
 function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const register = async (e) => {
+        e.preventDefault();
+        await fetch('http://localhost:3001/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+
   return (
-    <form className='register'>
+    <form className='register' onSubmit={register}>
         <h1>Register</h1>
-    <input type="text" placeholder="Username" />
-    <input type="password" placeholder="Password" />
+    <input type="text" 
+           placeholder="Username" 
+           value={username} 
+           onChange={e => setUsername(e.target.value)}/>
+    <input type="password" 
+           placeholder="Password"
+           value={password}
+           onChange={e => setPassword(e.target.value)}/>
     <button>Register</button>
 </form>
   )
