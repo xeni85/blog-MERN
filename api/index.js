0c88b28e389506be1f21304b5d2f4212afbd09b5
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-22 16:27:35 -0400

    fixed the crashing cannot fix the image

--- api/index.js
+++ api/index.js
@@ -97,12 +97,12 @@ app.post('/post', upload.single('file'), async (req, res) => {
         const {token} = req.cookies;
         jwt.verify(token, secret, {}, async (err, decoded) => {
             if(err) throw err;
-            const {title, description, content, urlToImg } = req.body;
+            const {title, description, content, urlToImg } = req.body;  
             const postDoc = await News.create({ 
                 title,
                 description,
                 content,
-                cover: urlToImg,
+                cover: urlToImg.substring(1, urlToImg.length() - 1),
                 author: decoded.id
             })
             console.log(req.body)
